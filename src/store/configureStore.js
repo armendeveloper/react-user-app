82c511f586ef74a59d3d46f80e69e75c4bd41d60
Armen Nalbandyan <armennalbandyan22@gmail.com>
2019-03-08 17:50:56 +0400

    fixed redux configuration

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,9 +3,9 @@ import {createStore} from "redux";
 import rootReducer  from "../reducers";
 
 export default () => {
-    const store = createStore(rootReducer);
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+    return createStore(rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
 
-    return store;
 
 }
